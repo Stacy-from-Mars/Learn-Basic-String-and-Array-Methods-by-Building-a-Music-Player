f058127c5a7e549d29c7a458db0ff52c49b5188f
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-04-19 23:02:52 +0300

    step 51

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ const renderSongs = array => {
 };
 
 const getCurrentSongIndex = () => {
-
+    return userData?.songs.indexOf(userData?.currentSong);
 };
 
 playButton.addEventListener("click", () => {
