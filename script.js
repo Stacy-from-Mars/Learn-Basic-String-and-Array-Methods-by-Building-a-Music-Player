d038d50cfc9ab78a57dac51c083d1f6ffda5a739
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-04-20 17:03:28 +0300

    step 65

--- script.js
+++ script.js
@@ -130,6 +130,9 @@ const highlightCurrentSong = () => {
     const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
     playlistSongElements.forEach((songEl) => {
         songEl.removeAttribute("aria-current");
+        if (songToHighlight) {
+            songToHighlight.setAttribute("aria-current", "true");
+        };
     });
 }
 
