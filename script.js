1c87ec3a403341911b2538883e773b891c11f42a
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-04-21 21:51:44 +0300

    step 75

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
     playButton.classList.add("playing");
     highlightCurrentSong();
     setPlayerDisplay();
+    setPlayButtonAccessibleText();
     audio.play();
 };
 
