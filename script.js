abf9eb74d5d35f6ed555f113c79d2f53c863f037
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-04-22 14:49:53 +0300

    step 95

--- script.js
+++ script.js
@@ -230,6 +230,8 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", ()=>{});
+
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
         if (a.title < b.title) {
